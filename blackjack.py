371d40796e805b40af0bd18eb48f32ff73ecacf1
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-04-13 20:29:48 +1000

    blackjack

--- blackjack.py
+++ blackjack.py
@@ -46,4 +46,160 @@ def main():
         print()
 
         # check if player has bust
-        #  cont line 59
+        if getHandValue(playerHand) > 21:
+            break
+
+        # get players move, H, S, or D
+        move = getMove(playerHand, money - bet)
+
+        # players actions
+        if move == 'D':
+            additionalBet = getBet(min(bet, (money -bet)))
+            bet += additionalBet
+            print('Bet increased to {}.' .format(bet))
+            print('Bet:', bet)
+
+        if move in ('H', 'D'):
+            #  Hit/ doubling down takes another card
+            newCard = deck.pop()
+            rank, suit = newCard
+            print('You drew a {} of {}.' .format(rank, suit))
+            playerHand.append(newCard)
+
+            if getHandValue(playerHand) > 21:
+                continue
+
+        if move in ('S', 'D'):
+            break
+    
+    # handle the dealers actions
+    if getHandValue(playerHand) <= 21:
+        while getHandValue(dealerHand) < 17:
+            print('Dealer hits...')
+            dealerHand.append(deck.pop())
+            displayHands(playerHand, dealerHand, False)
+
+            if getHandValue(dealerHand) > 21 :
+                input('Press Enter to continue...')
+                print('\n\n')
+
+    # show the final hands
+    displayHands(playerHand, dealerHand, True)
+
+    playerValue= getHandValue(playerHand)
+    dealerValue= getHandValue(dealerHand)
+    # Handle whether the palyer won, lost, tied
+    if dealerValue > 21:
+        print('Dealer busts! You win ${}!' .format(bet))
+        money += bet
+    elif (playerValue > 21) or (playerValue < dealerValue):
+        print('You lost!')
+        money -= bet
+    elif playerValue > dealerValue:
+        print('You won ${}!' .format(bet))
+        money += bet
+    elif playerValue == dealerValue:
+        print('It\'s a tie, the bet is returned to you.')
+
+    input('Press Enter to continue...')
+    print('\n\n')
+
+def getBet(maxBet):
+    while True:
+        print('How much do you want to bet? (1-{}, or QUIT)' .format(maxBet))
+        bet = input ('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.ext()
+        
+        if not bet.isdecimal():
+            continue
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet
+
+def getDeck():
+    deck =[]
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range (2,11):
+            deck.append((str(rank), suit))
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit))
+        random.shuffle(deck)
+        return deck
+    
+def displayHands (playerHand, dealerHand, showDealerHand):
+    print()
+    if showDealerHand:
+        print('DEALER:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        # hide dealers first card
+        displayCards([BACKSIDE] + dealerHand[1:])
+
+    # show players cards
+        print ('PLAYER:', getHandValue(playerHand))
+        displayCards(playerHand)
+    
+def getHandValue(cards):
+    value = 0
+    numberOfAces = 0
+
+    for card in cards:
+        rank = card[0]
+        if rank == 'A':
+            numberOfAces += 1
+        elif rank in ('K', 'Q', 'J'):
+            value += 10
+        else:
+            value += int(rank)
+
+    value += numberOfAces
+    for i in range(numberOfAces):
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+def displayCards (cards):
+
+    rows=['',' ',' ',' ','']
+
+    for i, card in enumerate(cards):
+        rows[0] += ' ___  '
+        if card == BACKSIDE:
+            rows[1] += ' |## | '
+            rows[2] += ' |###| '
+            rows[3] += ' |_##| '
+        else:
+            rank, suit = card
+            rows[1] += ' |{} | ' .format (rank.ljust(2))
+            rows[2] += ' | {} | ' .format(suit)
+            rows[3] += ' |_{}| ' .format(rank.rjust(2, '_'))
+
+    for row in rows:
+        print(row)
+
+def getMove(playerHand, money):
+
+     while True:  # Keep looping until the player enters a correct move.
+         # Determine what moves the player can make:
+         moves = ['(H)it', '(S)tand']
+ 
+         # The player can double down on their first move, which we can
+         # tell because they'll have exactly two cards:
+         if len(playerHand) == 2 and money > 0:
+             moves.append('(D)ouble down')
+ 
+         # Get the player's move:
+         movePrompt = ', '.join(moves) + '> '
+         move = input(movePrompt).upper()
+         if move in ('H', 'S'):
+             return move  # Player has entered a valid move.
+         if move == 'D' and '(D)ouble down' in moves:
+             return move  # Player has entered a valid move.
+            
+if __name__ == '__main__':
+    main()
